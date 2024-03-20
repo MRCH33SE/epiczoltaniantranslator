bbf4bb71a8cf2e8cb213a1119bce531f53021f93
The totally real human <caiusfraser@gmail.com>
2024-03-20 17:37:40 -0600

    added encode function :) yayyyyy

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+const english = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", " "];
+const zoltanian = ["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Cu", " "];
+
+function encode(message) {
+    let encodedMessage = "";
+    for(let i = 0; i < message.length; i++) {
+        let char =      message.charAt(i);
+        let       index = english.indexOf(char);
+        let translatechar = zoltanian[index];
+
+        encodedMessage = encodedMessage + translatechar;
+    }
+    return encodedMessage;
+}
+
 function decode(message){
-    const english = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-    const zoltanian = ["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Cu"];
     let decodedMessage = "";
     for (let i = 0; i < message.length; i++){
         let char =        message.charAt(i);
-        if (char == " "){
-            decodedMessage = decodedMessage + char;
-            continue;
-        }
         let nextChar = message.charAt(i + 1);
         if(nextChar != ' ' && nextChar == nextChar.toLowerCase()){
             char = char + message.charAt(i + 1);
@@ -30,3 +39,11 @@ let encodedMessage = "OHSSMn HeFArCaOBeHMnCu MnPSc HArB RfDs";
 console.log(encodedMessage);
 let decodedMessage = decode(encodedMessage);
 console.log(decodedMessage);
+let reincodedmessage = encode(decodedMessage);
+console.log(reincodedmessage);
+if(encodedMessage == reincodedmessage) {
+
+    console.log("pass");
+} else {
+    console.log("fail");
+}
